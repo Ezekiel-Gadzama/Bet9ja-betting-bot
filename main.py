7baea8aeb8a00bb2f28bda7e86c48d1ed7d2ee4d
Gadzama Ezekiel <71455903+Ezekiel-Gadzama@users.noreply.github.com>
2024-05-28 00:54:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,54 +1,21 @@
-import time
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
-
-# Driver setup
-firefox_options = Options()
-firefox_options.binary_location = '/usr/bin/firefox'  # Set Firefox binary location
-firefox_options.headless = True
 
 
-driver = webdriver.Firefox()
+import requests
 
-# Credentials
-username = "ezekielgadzama"
-password = "Ezekiel23"
+# Specify the URL of the website you want to open
+url = "https://sports.bet9ja.com/sport/soccer/1"
 
-# Navigation and login
-driver.get("https://sports.bet9ja.com/")
+# Create a session and mount the HTTPS protocol with an HTTPAdapter configured for HTTP/1.1
+session = requests.Session()
+adapter = requests.adapters.HTTPAdapter(pool_connections=10, pool_maxsize=10, max_retries=3, pool_block=False)
+session.mount("https://", adapter)
 
-username_id = 'username'
-password_id = 'password'
-login_popup_selector = '#header_item > div > div > div > div.h-ml__acc > div:nth-child(3) > div.btn-primary-m.btn-login'
+# Make a GET request to the URL with HTTP/1.1
+response = session.get(url)
 
-try:
-    element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, login_popup_selector)))
-    element.click()
-
-    # Wait for the input field to be visible
-    username_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="username"]')))
-    password_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="password"]')))
-
-    # Input credentials
-    username_field.send_keys(username)
-    password_field.send_keys(password)
-
-    # Click login button
-    login_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#header_item > div > div > div > div.h-ml__acc > div:nth-child(3) > div.login_popup.open > div.form > div.btn-primary-l.mt20')))
-    login_button.click()
-    print("Login successful")
-
-    # Add more interactions as needed...
-
-except TimeoutException:
-    print("Timeout occurred while waiting for elements.")
-except Exception as e:
-    print(f"An error occurred: {e}")
-finally:
-    # Keep the browser open for inspection
-    input("Press Enter to close the browser...")
-    driver.quit()
+# Check if the request was successful
+if response.status_code == 200:
+    # Print the content of the website
+    print(response.text)
+else:
+    print("Failed to fetch the website. Status code:", response.status_code)
