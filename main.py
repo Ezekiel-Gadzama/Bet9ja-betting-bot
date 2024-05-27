a3410871a921780e030d80463dd5307a6880c4d7
Gadzama Ezekiel <71455903+Ezekiel-Gadzama@users.noreply.github.com>
2024-05-28 00:37:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,106 +1,52 @@
 import time
-import subprocess
-import tempfile
-import os
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import StaleElementReferenceException, TimeoutException
+from selenium.common.exceptions import TimeoutException
 
-# mitmproxy script as a string
-mitmproxy_script = """
-from mitmproxy import http
+# Driver setup
+firefox_options = Options()
+firefox_options.headless = True
 
-def request(flow: http.HTTPFlow) -> None:
-    # Modify the request to use HTTP/1.1
-    flow.request.http_version = "HTTP/1.1"
-"""
+driver = webdriver.Firefox(options=firefox_options)
 
-# Create a temporary file for the mitmproxy script
-with tempfile.NamedTemporaryFile(delete=False, suffix=".py") as temp_script:
-    temp_script.write(mitmproxy_script.encode('utf-8'))
-    temp_script_path = temp_script.name
+# Credentials
+username = "ezekielgadzama"
+password = "Ezekiel23"
 
-# Start mitmproxy with the script
-proxy_process = subprocess.Popen(['mitmdump', '-s', temp_script_path, '--listen-port', '8080'])
+# Navigation and login
+driver.get("https://sports.bet9ja.com/")
+
+username_id = 'username'
+password_id = 'password'
+login_popup_selector = '#header_item > div > div > div > div.h-ml__acc > div:nth-child(3) > div.btn-primary-m.btn-login'
 
 try:
-    # Path to mitmproxy CA certificate
-    mitmproxy_ca_cert_path = os.path.expanduser('~/.mitmproxy/mitmproxy-ca-cert.pem')
+    element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, login_popup_selector)))
+    element.click()
 
-    # Driver setup
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--disable-popup-blocking")
-    chrome_options.add_argument("--disable-notifications")
+    # Wait for the input field to be visible
+    username_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="username"]')))
+    password_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="password"]')))
 
-    # Add proxy settings
-    chrome_options.add_argument('--proxy-server=http://localhost:8080')
+    # Input credentials
+    username_field.send_keys(username)
+    password_field.send_keys(password)
 
-    # Add the mitmproxy CA certificate to Chrome options
-    chrome_options.add_argument('--ignore-certificate-errors')
-    chrome_options.add_argument('--ignore-ssl-errors')
-    chrome_options.add_argument(f'--ssl-client-certificate={mitmproxy_ca_cert_path}')
+    # Click login button
+    login_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#header_item > div > div > div > div.h-ml__acc > div:nth-child(3) > div.login_popup.open > div.form > div.btn-primary-l.mt20')))
+    login_button.click()
+    print("login sucessful")
 
-    # Initialize the WebDriver with options
-    driver = webdriver.Chrome(options=chrome_options)
+    # Add more interactions as needed...
 
-    # Credentials
-    username = "ezekielgadzama"
-    password = "Ezekiel23"
-
-    # Navigation and login
-    driver.set_window_size(1920, 1080)
-    driver.get("https://sports.bet9ja.com/")
-
-    login_popup_selector = '#header_item > div > div > div > div.h-ml__acc > div:nth-child(3) > div.btn-primary-m.btn-login'
-    login_xpath = '//*[@id="header_item"]/div/div/div/div[2]/div[3]/div[2]/div[1]/div[3]'
-
-    # Retry logic to handle StaleElementReferenceException
-    time.sleep(5)
-    max_retries = 3
-
-    try:
-        element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, login_popup_selector)))
-        element.click()
-
-        # Wait for the input field to be visible
-        username_field = WebDriverWait(driver, 10).until(
-            EC.visibility_of_element_located((By.XPATH, '//*[@id="username"]'))
-        )
-        password_field = WebDriverWait(driver, 10).until(
-            EC.visibility_of_element_located((By.XPATH, '//*[@id="password"]'))
-        )
-
-        # Click the input field
-        username_field.click()
-        time.sleep(3)
-        username_field.click()
-        username_field.send_keys(username)
-
-        password_field.click()
-        time.sleep(3)
-        password_field.click()
-        password_field.send_keys(password)
-
-        login = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#header_item > div > div > div > div.h-ml_acc > div:nth-child(3) > div.login_popup.open > div.form > div.btn-primary-l.mt20')))
-        login.click()
-
-        # Add more interactions as needed...
-
-    except Exception as e:
-        print(f"An error occurred: {e}")
-    finally:
-        # Keep the browser open for inspection
-        input("Press Enter to close the browser...")
-        driver.quit()
-
+except TimeoutException:
+    print("Timeout occurred while waiting for elements.")
+except Exception as e:
+    print(f"An error occurred: {e}")
 finally:
-    # Ensure mitmproxy is terminated
-    proxy_process.terminate()
-    # Remove the temporary mitmproxy script
-    os.remove(temp_script_path)
+    # Keep the browser open for inspection
+    input("Press Enter to close the browser...")
+    driver.quit()
